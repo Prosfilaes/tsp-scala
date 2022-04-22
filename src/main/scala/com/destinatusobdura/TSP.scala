41f8abe5670c87f492f150ee39d265d027e626af
David Starner <prosfilaes@gmail.com>
2022-04-21 19:45:37 -0500

    2-opt

--- src/main/scala/com/destinatusobdura/TSP.scala
+++ src/main/scala/com/destinatusobdura/TSP.scala
@@ -1,5 +1,6 @@
 import scala.math._
 import scala.collection.parallel.CollectionConverters._
+import scala.annotation.tailrec
 import java.util.concurrent.atomic.AtomicInteger
 
 package com.destinatusobdura {
@@ -26,8 +27,44 @@ case class TSPProblem (nodes : Int, dist : Array[Array[Int]]) {
 
   val bestSol = new AtomicInteger (TSPProblem.measureTSP (nearestNeighborSol, dist))
 
+  private def twoOptSwap (route : Seq[Int], i : Int, j : Int) : Seq[Int] = {
+    val (head, tail) = route.splitAt (i)
+    val (middle, end) = tail.splitAt (j - i)
+    val ret = head ++: middle.reverse ++: end
+    //assert (ret.length == route.length)
+    ret
+  }
+
+  private def twoOptLength (route : Seq[Int], origLen : Int, i : Int, j : Int) : Int = {
+    //println (i.toString + "/" + j.toString)
+    assert (j - i > 2)
+    // Old length - dist (i - 1, i) - dist (j - 1, j) + dist (i - 1, j - 1) + dist (i, j)
+    val newLen = origLen - dist (route (i - 1))(route(i)) - dist (route (j - 1))(route (j)) 
+      + dist (route (i - 1))(route (j - 1)) + dist (route (i))(route(j))
+    //assert (newLen == TSPProblem.measureTSP (twoOptSwap(route, i, j), dist))
+    newLen
+  }
+
+  @tailrec final def twoOpt (route : Seq[Int], oL : Int = -1, i : Int = 1, j : Int = 4) : Seq[Int] = {
+    val origLen = if (oL > 0) oL else TSPProblem.measureTSP (route, dist)
+    if (j >= route.length) twoOpt (route, origLen, i + 1, i + 4)
+    else if (i >= route.length) route
+    else {
+      val twoLen = twoOptLength (route, origLen, i, j)
+      if (twoLen < origLen) twoOpt (twoOptSwap (route, i, j), twoLen)
+      else twoOpt (route, origLen, i, j + 1)
+    }
+  }
+
   def bnb (b : Int) : Seq[Int] = {
-    var best = b
+    if (b < bestSol.get()) {
+        var continue = true
+        var currBest = bestSol.get()
+        while (continue && !bestSol.compareAndSet(currBest, b)) {
+          currBest = bestSol.get()
+          if (b >= currBest) continue = false
+        }
+    }
     val nodeSet = Range (0, nodes).toSet
     val mins = dist.map (_.filter(_ != 0).sorted.take(2).sum / 2)
     def bnbRec (start : Seq[Int]) : Option[(Seq[Int], Int)] = {
@@ -125,7 +162,7 @@ object TSPProblem {
       else if (lines.contains ("EOF")) {
         break = true
       }
-      else if (lines.startsWith (" ")) {
+      else if (lines.startsWith (" ") || lines.startsWith ("0")) {
         val split = lines.strip.split ("  *")
         locs += ((split(1).toDouble, split(2).toDouble))
       }
@@ -141,14 +178,32 @@ object TSP {
 
   def main(args: Array[String]) =  {
     val prob = TSPProblem.fromGeoFile (args(0))
+    //println (prob)
+    //println (prob.dist.toSeq.map (_.toSeq))
     println ("Longest distance: " + prob.dist.map (_.max).max.toString)
+
+    val bestTwo = prob.twoOpt (Range(0, prob.nodes).toSeq)
+    val bestTwodist = TSPProblem.measureTSP(bestTwo, prob.dist)
+
+    System.out.println ("2-Opt of 0-14: ")
+    System.out.println (bestTwo.toString + " : " + bestTwodist)
+
     val nn = prob.nearestNeighborSol
     val nndist = TSPProblem.measureTSP(nn, prob.dist)
 
     System.out.println ("Nearest neighbor: ")
     System.out.println (nn.toString + " : " + nndist)
 
-    val bnb = prob.bnb (nndist)
+    val two = prob.twoOpt (nn)
+    val twodist = TSPProblem.measureTSP(two, prob.dist)
+
+    System.out.println ("2-Opt of NN: ")
+    System.out.println (two.toString + " : " + twodist)
+
+    var minimum = Math.min (Math.min(nndist, twodist), bestTwodist)
+    System.out.println ("Minimum: " + minimum.toString)
+    System.out.println ("--")
+    val bnb = prob.bnb (minimum)
     System.out.println ("Final solution: " + bnb.toString)
     System.out.println ("Final distance: " + TSPProblem.measureTSP(bnb, prob.dist).toString)
   }
